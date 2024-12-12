dad1261babd5ae2b9eeea559d463f5ee49dca8c9
Virtual Jasper <jasper2virtual@hotmail.com>
2024-12-13 00:31:10 +0800

    Refactor Card component to use semantic HTML and improve accessibility

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -2,25 +2,25 @@
     <div class="bg-white p-8 rounded-xl flex flex-col w-full max-w-[600px] gap-y-8">
         <div class="flex gap-8">
             <img src="/src/assets/images/icon-star.svg" alt="star" class="w-[2.5rem] aspect-square"/>
-            <span class="app-text-display">FAQs</span>
+            <h1 class="app-text-display">FAQs</h1>
         </div>
-        <div class="flex flex-col gap-8">
+        <main class="flex flex-col gap-8">
             <div v-for="(faq,key) in faqs" :key class="grid duration-500 transition-[grid-template-rows] grid-rows-[auto_0fr] has-[:checked]:grid-rows-[auto_1fr] gap-y-4">
                 <label class="app-text-title flex items-center cursor-pointer group hover:text-app-pink">
                     <input type="radio" :value="key" v-model="picked" class="appearance-none">{{ faq.question }} 
                     <div class="grid grid-cols-1 grid-rows-1 ml-auto shrink-0">
-                        <img class="row-span-full col-span-full group-has-[:checked]:opacity-0 duration-500 transition-opacity" src="/src/assets/images/icon-plus.svg">
-                        <img class="row-span-full col-span-full opacity-0 group-has-[:checked]:opacity-100 duration-500 transition-opacity" src="/src/assets/images/icon-minus.svg">
+                        <img class="row-span-full col-span-full group-has-[:checked]:opacity-0 duration-500 transition-opacity" src="/src/assets/images/icon-plus.svg" alt="icon plus">
+                        <img class="row-span-full col-span-full opacity-0 group-has-[:checked]:opacity-100 duration-500 transition-opacity" src="/src/assets/images/icon-minus.svg" alt="icon minus">
                     </div>  
                 </label>
                 <p class="app-text-body text-app-pale-purple overflow-y-hidden">{{ faq.answer }}</p>
             </div>
-        </div>
+        </main>
     </div>
 </template>
 
 <script lang="ts" setup>
-import { ref, Transition } from 'vue'
+import { ref } from 'vue'
 const faqs = [
     {
         question: "What is Frontend Mentor, and how will it help me?",
